7da1610bfe8304a5f39ee9c6e268c37bc86b32b8
unknown <akshay.varma1@gmail.com>
2018-12-20 16:50:02 +0530

    show more details apart from sni

--- mitmproxy/proxy/root_context.py
+++ mitmproxy/proxy/root_context.py
@@ -51,7 +51,7 @@ class RootContext:
         # 1. check for --ignore
         if self.config.check_ignore:
             ignore = self.config.check_ignore(top_layer.server_conn.address)
-            print("server_conn.address", top_layer.server_conn.address)
+            print("server_conn.address", top_layer.server_conn.address, top_layer)
             if not ignore and client_tls:
                 try:
                     client_hello = tls.ClientHello.from_file(self.client_conn.rfile)
